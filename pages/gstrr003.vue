5a472023cd2b38d12aae1313afe49799562deca9
Latte <watthana_pee@freewillsolutions.com>
2025-08-07 10:35:01 +0700

    create new page gstrr003

--- pages/gstrr003.vue
+++ pages/gstrr003.vue
@@ -21,29 +21,58 @@
         :items="dataItems"
         density="compact"
         fixed-header
-        class="min-w-max border rounded-sm text-xs"
+        class="min-w-max"
       >
-        <template
-          v-for="col in headers"
-          v-slot:[`header.${col.key}`]="{ column }"
-          :key="col.key"
-        >
-          <span
-            class="text-xs font-semibold"
-            :class="{
-              'text-left': col.align === 'start',
-              'text-center': col.align === 'center',
-              'text-right': col.align === 'end',
-            }"
-            >{{ column.title }}</span
-          >
+        <template v-slot:top>
+          <v-expand-transition>
+            <div v-if="headers.length < 18">
+              <v-btn
+                class="mb-2"
+                rounded="lg"
+                size="small"
+                text="Reset"
+                variant="text"
+                block
+                border
+                @click="onClickReset"
+              ></v-btn>
+            </div>
+          </v-expand-transition>
         </template>
 
+        <!-- * Header Row -->
+        <template v-slot:headers="{ columns }">
+          <tr class="border-b-2 border-gray-100">
+            <template v-for="(col, idx) in columns" :key="col.key ?? ''">
+              <th>
+                <span
+                  class="text-xs font-semibold"
+                  :class="{
+                    'text-left': col.align === 'start',
+                    'text-center': col.align === 'center',
+                    'text-right': col.align === 'end',
+                  }"
+                  >{{ col.title }}</span
+                >
+                <v-icon
+                  v-if="headers[idx] && headers[idx].removable"
+                  color="medium-emphasis"
+                  icon="$close"
+                  @click="remove(col.key ?? '')"
+                ></v-icon>
+              </th>
+            </template>
+          </tr>
+        </template>
+
+        <!-- * Body Rows -->
         <template v-slot:item="{ item, columns }">
-          <tr>
+          <tr
+            class="hover:bg-gray-50 cursor-pointer border-b-2 border-gray-100"
+          >
             <td
               v-for="col in columns"
-              :key="col.key"
+              :key="col.key || ''"
               class="text-xs"
               :class="{
                 'text-left': col.align === 'start',
@@ -51,14 +80,15 @@
                 'text-right': col.align === 'end',
               }"
             >
-              {{ item[col.key] }}
+              {{ col.key ? item[col.key] : "" }}
             </td>
           </tr>
         </template>
 
-        <!-- Sum Row -->
+        <!-- * Sum Row -->
         <template v-slot:body.append>
-          <tr class="bg-gray-100 font-bold">
+          <!-- ? THB -->
+          <tr class="font-bold">
             <td
               v-for="(col, idx) in headers"
               :key="col.key"
@@ -69,12 +99,52 @@
                 'text-right': col.align === 'end',
               }"
             >
-              <!-- Show "Total" in first column, sum for numeric columns, blank otherwise -->
-              <!-- <span v-if="idx === 0">Total</span>
-              <span v-else-if="isSumColumn(col.key)">
-                {{ sumColumn(col.key) }}
-              </span> -->
-              <span>hello</span>
+              <span v-if="col.key === 'name_en'">Total</span>
+              <span v-else-if="col.key === 'currency'">THB</span>
+              <span v-else-if="col.key === 'avail_dr'">0</span>
+              <span v-else-if="col.key === 'avail_cr'">0</span>
+              <span v-else-if="col.key === 'pending_buy'">0</span>
+              <span v-else-if="col.key === 'pending_sell'">0</span>
+              <span v-else-if="col.key === 'ledger_dr'">0</span>
+              <span v-else-if="col.key === 'total_settle'">0</span>
+              <span v-else-if="col.key === 'short_sell'">0</span>
+              <span v-else-if="col.key === 'trust_bl_dr'">0</span>
+              <span v-else-if="col.key === 'trust_avail_cr'">0</span>
+              <span v-else-if="col.key === 'unckeck_bl'">0</span>
+              <span v-else-if="col.key === 'net_trust_dr'">0</span>
+              <span v-else-if="col.key === 'net_trust_cr'">0</span>
+              <span v-else-if="col.key === 'prop_account'">0</span>
+            </td>
+          </tr>
+
+          <!-- ? USD -->
+          <tr class="font-bold">
+            <td
+              v-for="(col, idx) in headers"
+              :key="col.key"
+              class="text-xs"
+              :class="{
+                'text-left': col.align === 'start',
+                'text-center': col.align === 'center',
+                'text-right': col.align === 'end',
+              }"
+            >
+              <span v-if="col.key === 'name_en'">Total</span>
+              <span v-else-if="col.key === 'currency'">USD</span>
+              <span v-else-if="col.key === 'currency'">THB</span>
+              <span v-else-if="col.key === 'avail_dr'">0</span>
+              <span v-else-if="col.key === 'avail_cr'">0</span>
+              <span v-else-if="col.key === 'pending_buy'">0</span>
+              <span v-else-if="col.key === 'pending_sell'">0</span>
+              <span v-else-if="col.key === 'ledger_dr'">0</span>
+              <span v-else-if="col.key === 'total_settle'">0</span>
+              <span v-else-if="col.key === 'short_sell'">0</span>
+              <span v-else-if="col.key === 'trust_bl_dr'">0</span>
+              <span v-else-if="col.key === 'trust_avail_cr'">0</span>
+              <span v-else-if="col.key === 'unckeck_bl'">0</span>
+              <span v-else-if="col.key === 'net_trust_dr'">0</span>
+              <span v-else-if="col.key === 'net_trust_cr'">0</span>
+              <span v-else-if="col.key === 'prop_account'">0</span>
             </td>
           </tr>
         </template>
@@ -83,71 +153,183 @@
   </v-container>
 </template>
 
-<script>
+<script lang="ts">
 import Items from "~/data/json/gstrr003.json";
 
 export default {
   data() {
     return {
-      headers: [
-        {
-          title: "Account Number",
-          align: "start",
-          key: "account_no",
-        },
-        { title: "Account Name", align: "start", key: "name_en" },
-        { title: "Account Type", align: "start", key: "account_type" },
-        { title: "Account Class", align: "center", key: "account_class" },
-        { title: "Currency", align: "center", key: "currency" },
-        { title: "Available Balance Dr", align: "end", key: "avail_dr" },
-        { title: "Available Balance Cr", align: "end", key: "avail_cr" },
-        { title: "Uncome Balance Dr", align: "end", key: "pending_buy" },
-        { title: "Uncome Balance Cr", align: "end", key: "pending_sell" },
-        { title: "Ledger Balance Dr", align: "end", key: "ledger_dr" },
-        { title: "Ledger Balance Cr", align: "end", key: "total_settle" },
-        {
-          title: "Short Selling Proceeds Being Withheld",
-          align: "end",
-          key: "short_sell",
-        },
-        { title: "Trust Balance Dr", align: "end", key: "trust_bl_dr" },
-        { title: "Trust Balance Cr", align: "end", key: "trust_avail_cr" },
-        { title: "Uncheck Balance", align: "end", key: "unckeck_bl" },
-        { title: "Net Trust Balance Dr", align: "end", key: "net_trust_dr" },
-        { title: "Net Trust Balance Cr", align: "end", key: "net_trust_cr" },
-        { title: "Proprietary Account", align: "center", key: "prop_account" },
-      ],
+      headers: [] as {
+        title: string;
+        align: "start" | "center" | "end";
+        key: string;
+        removable: boolean;
+      }[],
+      dataItems: [] as { [key: string]: any }[],
 
-      dataItems: [],
-      //   dataItems: [
-      //     {
-      //       account_no: "1-2310000029-1",
-      //       name_en: "Testonetwo Wealthtest",
-      //       account_type: "CASH",
-      //       account_class: "CUSTOMER",
-      //       currency: "THB",
-      //       avail_dr: "0",
-      //       avail_cr: "2,000,000.0",
-      //       pending_buy: "0",
-      //       pending_sell: "0",
-      //       ledger_dr: "0",
-      //       total_settle: "2,000,000.0",
-      //       short_sell: "0",
-      //       trust_bl_dr: "2,000,000.0",
-      //       trust_avail_cr: "2,000,000.0",
-      //       unckeck_bl: "0",
-      //       net_trust_dr: "0",
-      //       net_trust_cr: "2,000,000.0",
-      //       prop_account: "N",
-      //       datetime: false,
-      //     },
-      //   ],
+      summaryTotal: {
+        thb: {
+          avail_dr: 0,
+          avail_cr: 0,
+          pending_buy: 0,
+          pending_sell: 0,
+          ledger_dr: 0,
+          total_settle: 0,
+          short_sell: 0,
+          trust_bl_dr: 0,
+          trust_avail_cr: 0,
+          unckeck_bl: 0,
+          net_trust_dr: 0,
+          net_trust_cr: 0,
+        },
+        usd: {
+          avail_dr: 0,
+          avail_cr: 0,
+          pending_buy: 0,
+          pending_sell: 0,
+          ledger_dr: 0,
+          total_settle: 0,
+          short_sell: 0,
+          trust_bl_dr: 0,
+          trust_avail_cr: 0,
+          unckeck_bl: 0,
+          net_trust_dr: 0,
+          net_trust_cr: 0,
+        },
+      },
     };
   },
   mounted() {
+    this.headers = this.fetchHeaders();
     this.dataItems = Items.dataList;
+  },
+  methods: {
+    fetchHeaders() {
+      return [
+        {
+          title: "Account Number",
+          align: "start" as "start",
+          key: "account_no",
+          removable: false,
+        },
+        {
+          title: "Account Name",
+          align: "start" as "start",
+          key: "name_en",
+          removable: false,
+        },
+        {
+          title: "Account Type",
+          align: "start" as "start",
+          key: "account_type",
+          removable: true,
+        },
+        {
+          title: "Account Class",
+          align: "center" as "center",
+          key: "account_class",
+          removable: true,
+        },
+        {
+          title: "Currency",
+          align: "center" as "center",
+          key: "currency",
+          removable: false,
+        },
+        {
+          title: "Available Balance Dr",
+          align: "end" as "end",
+          key: "avail_dr",
+          removable: true,
+        },
+        {
+          title: "Available Balance Cr",
+          align: "end" as "end",
+          key: "avail_cr",
+          removable: true,
+        },
+        {
+          title: "Uncome Balance Dr",
+          align: "end" as "end",
+          key: "pending_buy",
+          removable: true,
+        },
+        {
+          title: "Uncome Balance Cr",
+          align: "end" as "end",
+          key: "pending_sell",
+          removable: true,
+        },
+        {
+          title: "Ledger Balance Dr",
+          align: "end" as "end",
+          key: "ledger_dr",
+          removable: true,
+        },
+        {
+          title: "Ledger Balance Cr",
+          align: "end" as "end",
+          key: "total_settle",
+          removable: true,
+        },
+        {
+          title: "Short Selling Proceeds Being Withheld",
+          align: "end" as "end",
+          key: "short_sell",
+          removable: true,
+        },
+        {
+          title: "Trust Balance Dr",
+          align: "end" as "end",
+          key: "trust_bl_dr",
+          removable: true,
+        },
+        {
+          title: "Trust Balance Cr",
+          align: "end" as "end",
+          key: "trust_avail_cr",
+          removable: true,
+        },
+        {
+          title: "Uncheck Balance",
+          align: "end" as "end",
+          key: "unckeck_bl",
+          removable: true,
+        },
+        {
+          title: "Net Trust Balance Dr",
+          align: "end" as "end",
+          key: "net_trust_dr",
+          removable: true,
+        },
+        {
+          title: "Net Trust Balance Cr",
+          align: "end" as "end",
+          key: "net_trust_cr",
+          removable: true,
+        },
+        {
+          title: "Proprietary Account",
+          align: "center" as "center",
+          key: "prop_account",
+          removable: true,
+        },
+      ];
+    },
+
+    onClickReset() {
+      this.headers = this.fetchHeaders();
+    },
+
+    remove(columnKey: string) {
+      this.headers = this.headers.filter((header) => header.key !== columnKey);
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.selected-row {
+  background-color: red;
+}
+</style>
